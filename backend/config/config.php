8451849f39bdd414fabc9a805c25826395ed70a2
Samir Poudel <samir39bit23@kcc.edu.np>
2026-04-06 18:00:20 +0545

    Modified Functions Security

--- backend/config/config.php
+++ backend/config/config.php
@@ -8,10 +8,10 @@ $db_name = 'medtracker_db';
 
 $smtp_host = 'smtp.gmail.com';
 $smtp_port = 587; // or 465 for SSL
-$smtp_username = 'brooskings661@gmail.com';
-$smtp_password = 'rcha tqxx iwzp axpl';   
-$smtp_from_email = 'brooskings661@gmail.com'; 
-$smtp_from_name = 'MedTracker Alerts';
+$smtp_username = '';
+$smtp_password = '';   
+$smtp_from_email = ''; 
+$smtp_from_name = '';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
